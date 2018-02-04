62c53837555c58a6c11836bc02e9141cb20468b7
glennsl <glenn@slotte.net>
2018-02-04 14:10:33 +0100

    use compiler generated with jsoo 3

--- webpack.config.js
+++ webpack.config.js
@@ -2,5 +2,9 @@ module.exports = {
   entry: './examples/browser.bs.js',
   output: {
     filename: 'examples/browserBundle.js'
+  },
+  node: {
+    fs: 'empty',
+    child_process: 'empty'
   }
 };
